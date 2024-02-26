6379f890e5e9af0fa91a5e2de6d81c3207f383ca
yehaitian0627 <71301962+yehaitian0627@users.noreply.github.com>
2024-02-26 12:05:40 +0800

    Add files via upload

---
+++ 8_ConvolutionTest.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn.functional as F
+
+# 简单的卷积模拟
+
+# 输入
+conv_input = torch.tensor([[1, 2, 4, 5, 7],
+                      [6, 3, 6, 1, 7],
+                      [2, 5, 7, 1, 4],
+                      [3, 4, 6, 2, 7],
+                      [2, 6, 7, 2, 6],])
+
+# 卷积核
+kernel = torch.tensor([[1, 4, 6],
+                       [4, 6, 2],
+                       [3, 7, 8]])
+
+# 将输入和卷积核变成 conv2d方法需要的形式
+conv_input = torch.reshape(conv_input, [1, 1, 5, 5])
+kernel = torch.reshape(kernel, [1, 1, 3, 3])
+
+# 进行卷积 参数为：输入、卷积核、步长（卷积核每次的移动距离、扩充边距（默认为 0））
+conv_output = F.conv2d(conv_input, kernel, stride=2, padding=1)
+
+print(conv_output)
